0de1c76ff57ab7d9c4ca65de3e8745ce840e1ea4
Jose Fernandez (magec) <jfernandezperez@gmail.com>
2010-05-20 14:32:13 +0200

    Added new mappings Now when a mapping is not found an exception is thrown

--- lib/icontrol/mappings.rb
+++ lib/icontrol/mappings.rb
@@ -6,10 +6,14 @@ module IControl
       when /string/ then result[:item]
       when /iControl:LocalLB\.ProfileMatchPatternStringArray/ then map_object(result[:item][:values])
       when /iControl:LocalLB\.MatchPatternString/ then [result[:item]].flatten
-      when /iControl:Common\.IPPortDefinition\[\]/ then [result[:item][:item]].flatten.map{|i| IControl::Common::IPPortDefinition.new(i)}
+      when /iControl:Common\.IPPortDefinition\[\]/ then [result[:item][:item]].flatten.map{|i| IControl::LocalLB::PoolMember.new(i)}
       when /iControl:LocalLB.LBMethod/ then [result[:item]].flatten
       when /VirtualServerProfileAttribute\[\]/ then [result[:item][:item]].flatten.map{|i| IControl::LocalLB::VirtualServer::VirtualServerProfileAttribute.new(i) }
-      when /iControl:LocalLB.VirtualServer.VirtualServerHttpClass\[\]/ then [result[:item][:item]].flatten.map{|i| IControl::LocalLB::ProfileHttpClass.new(i) }
+      when /iControl:LocalLB.VirtualServer.VirtualServerHttpClass\[\]/ then [result[:item][:item]].flatten.map{|i| IControl::LocalLB::ProfileHttpClass.new(i)}
+      when /iControl:LocalLB\.ProfileString\[/ then [result[:item]].flatten.map{|i| IControl::LocalLB::Pool.new(i.merge({:id => i[:value] }) ) }
+      when /iControl:LocalLB\.PoolMember\.MemberObjectStatus\[\]\[/ then result[:item][:item].map{|j| IControl::LocalLB::PoolMember.new(j[:member].merge(:status => j[:object_status])  ) }
+      else
+        raise "No type matching found"
       end
     end
   end
