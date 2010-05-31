5cc53e851018a8500d20f88136034d5f38ff21ba
Jose Fernandez (magec) <jfernandezperez@gmail.com>
2010-05-31 13:10:04 +0200

    Added the clone method

--- lib/icontrol/local_lb/pool.rb
+++ lib/icontrol/local_lb/pool.rb
@@ -13,15 +13,54 @@ class IControl::LocalLB::Pool
     return pool_info.status if pool_info
   end
 
-    # This method clones a pool with another name (id)
+  def lb_method
+    aux = super
+    return aux.first if aux.respond_to? 'first'
+    aux
+  end
+
+  # This method clones a pool with another name (id)
   def clone(new_id)
-    return IControl::LocalLB::Pool.create(:id => new_id, :lb_methods => lb_method, :members => member)
+    prv_monitor_rule = monitor_rule
+    new_pool = IControl::LocalLB::Pool.create(:id => new_id, :lb_method => lb_method, :members => member)
+    new_pool.monitor_rule = prv_monitor_rule
+    return new_pool
   end
   
+  # Returns a collection of the monitors associated with this pool
+  def monitor_rule
+    return monitor_association
+  end
+
+  def monitor_rule=(new_monitor_rule)
+    item = 0
+
+    templates = new_monitor_rule.monitor_templates.map { |i| {:"item#{item+=1}" => i}}
+    rule_type = ( templates.length > 1 ) ? "MONITOR_RULE_TYPE_AND_LIST" : "MONITOR_RULE_TYPE_SINGLE"
+    response = IControl::LocalLB::Pool.set_monitor_association do |soap|
+      soap.body = {
+        "monitor_associations" => {
+          "item" => {
+            "pool_name" => @attributes[:id] ,
+
+            "monitor_rule" => {
+              :type => rule_type,
+              :quorum => new_monitor_rule.quorum,
+              "monitor_templates" => templates
+            }
+          }
+
+        }
+      }
+    end
+  end
+
   def self.create(attributes)    
     response = super do |soap|      
       item = 0
-      members = attributes[:members].map{ |i| {:address => i.address, :port => i.port } }.map { |i| {:"item#{item+=1}" => i}}
+      members = {}
+     attributes[:members].each{ |i| members[:"item#{item+=1}"] = {:address => i.address, :port => i.port } }
+
       soap.body = {
         "pool_names" => {:item => attributes[:id] },
         "lb_methods" => {:item => attributes[:lb_method]},
