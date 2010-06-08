95cd16c4d52974db2e9dea97dfdb355ee7f18cfd
Jose Fernandez (magec) <jfernandezperez@gmail.com>
2010-06-08 11:30:04 +0200

    Added tests for the VirtualServer class

--- spec/icontrol_local_lb_virtual_server_spec.rb
+++ spec/icontrol_local_lb_virtual_server_spec.rb
@@ -4,6 +4,7 @@ describe IControl::LocalLB::VirtualServer do
 
   before do 
     @virtual_server = IControl::LocalLB::VirtualServer.find("test_virtual_server")
+    @virtual_server_nil = IControl::LocalLB::VirtualServer.find("test_virtual_server_nil")
   end
 
   describe "find method" do
@@ -42,18 +43,216 @@ describe IControl::LocalLB::VirtualServer do
     
   end
 
-
   describe "http_class_profiles method" do
 
     it "should exist" do 
-
       @virtual_server.class.should be(IControl::LocalLB::VirtualServer)
-      
+     
       http_class_profiles = @virtual_server.http_class_profiles
       http_class_profiles.should_not be(nil)
       http_class_profiles.class.should be(Array)
       http_class_profiles.first.class.should be(IControl::LocalLB::ProfileHttpClass)
       http_class_profiles.first.id.should_not be(nil)
     end  
+  end
+
+  describe "wildmask method" do 
+    it "should exists" do
+      @virtual_server.wildmask.class.should be(String)
+    end
+  end
+
+  describe "type method" do 
+
+    it "shold exists" do 
+      lambda { @virtual_server.type }.should_not raise_exception(NoMethodError)
+    end
+
+    it "should return a constant child of VirtualServerType" do
+      vs_type = @virtual_server.type
+      vs_type.should_not be(nil)
+      vs_type.ancestors.should include(IControl::LocalLB::VirtualServer::VirtualServerType)
+    end
+  end
+
+  describe "cmp_enable_mode method" do 
+
+    it "should exists" do 
+      lambda { @virtual_server.cmp_enable_mode }.should_not raise_exception(NoMethodError)
+    end
+
+    it "should return a constant telling the cmp_enable_mode of the virtual server" do
+      cmp_enable = @virtual_server.cmp_enable_mode
+      cmp_enable.should_not be(nil)
+      cmp_enable.ancestors.should include(IControl::LocalLB::VirtualServer::VirtualServerCMPEnableMode)
+    end
+  end
+
+  describe "destination method" do 
+    it "should exists" do
+      lambda { @virtual_server.destination }.should_not raise_exception(NoMethodError)
+    end
+
+    it "should return an IPPortDefinition instance" do 
+      @virtual_server.destination.class.should be(IControl::Common::IPPortDefinition)
+    end
+  end
+
+  describe "protocol method" do 
+    it "should exists" do
+      lambda { @virtual_server.protocol }.should_not raise_exception(NoMethodError)
+    end
+    
+    it "should return an IControl::Common::ProtocolType constant" do 
+      @virtual_server.protocol.ancestors.should include(IControl::Common::ProtocolType)
+    end
+  end
+
+  describe "enabled_state method" do
+    it "should exists" do
+      lambda { @virtual_server.enabled_state }.should_not raise_exception(NoMethodError)
+    end
+
+    it "should return an IControl::Common::EnabledState constant" do 
+        @virtual_server.enabled_state.ancestors.should include(IControl::Common::EnabledState)
+    end    
+  end
+
+  describe "connection_limit" do
+    it "should exists" do
+      lambda { @virtual_server.connection_limit }.should_not raise_exception(NoMethodError)
+    end
+
+    it "should return an IControl::Common::ULong64 instance" do 
+      @virtual_server.connection_limit.class.should be(IControl::Common::ULong64)
+    end    
+  end
+
+  describe "rate_class method" do
+    it "should exists" do
+      lambda { @virtual_server.rate_class }.should_not raise_exception(NoMethodError)
+    end
+
+    it "should return an IControl::LocalLB::RateClass instance" do
+      pending
+    end    
+
+    it "should return nil if there is not a rate class associated with the virtual_server" do 
+      @virtual_server_nil.rate_class.should be(nil)
+    end
+  end
+
+  describe "connection_mirror_state method" do
+    it "should exists" do
+      lambda { @virtual_server.connection_mirror_state }.should_not raise_exception(NoMethodError)
+    end
+
+    it "should return an IControl::Common::EnabledState constant" do
+      @virtual_server.connection_mirror_state.ancestors.should include(IControl::Common::EnabledState)
+    end    
+  end
+
+  describe "translate_port_state method" do
+    it "should exists" do
+      lambda { @virtual_server.translate_port_state }.should_not raise_exception(NoMethodError)
+    end
+
+    it "should return an IControl::Common::EnabledState constant" do
+      @virtual_server.translate_port_state.ancestors.should include(IControl::Common::EnabledState)
+    end    
+  end
+
+  describe "translate_address_state method" do
+    it "should exists" do
+      lambda { @virtual_server.translate_address_state }.should_not raise_exception(NoMethodError)
+    end
+
+    it "should return an IControl::Common::EnabledState constant" do
+      @virtual_server.translate_address_state.ancestors.should include(IControl::Common::EnabledState)
+    end    
+  end
+
+  describe "sorce_port_behavior method" do
+    it "should exists" do
+      lambda { @virtual_server.source_port_behavior }.should_not raise_exception(NoMethodError)
+    end
+
+    it "should return an IControl::Common::SourcePortBehavior constant" do
+      @virtual_server.source_port_behavior.ancestors.should include(IControl::Common::SourcePortBehavior)
+    end    
+  end
+
+  describe "last_hop_pool method" do
+    it "should exists" do
+      lambda { @virtual_server.last_hop_pool }.should_not raise_exception(NoMethodError)
+    end
+
+    it "should return an IControl::LocalLB::Pool instance" do
+      @virtual_server.last_hop_pool.class.should be(IControl::LocalLB::Pool)
+    end    
+  end
+
+  describe "actual_hardware_acceleration method" do
+    it "should exists" do
+      lambda { @virtual_server.actual_hardware_acceleration }.should_not raise_exception(NoMethodError)
+    end
+
+    it "should return an IControl::LocalLB::HardwareAccelerationMode instance" do
+      @virtual_server.actual_hardware_acceleration.ancestors.should include(IControl::LocalLB::HardwareAccelerationMode)
+    end    
+
+  end
+
+  describe "snat_type method" do
+    it "should exists" do
+      lambda { @virtual_server.snat_type }.should_not raise_exception(NoMethodError)
+    end
+
+    it "should return an IControl::LocalLB::SnatType constant" do
+      @virtual_server.snat_type.ancestors.should include(IControl::LocalLB::SnatType)
+    end    
+  end
+
+  describe "snat_pool method" do
+    it "should exists" do
+      lambda { @virtual_server.snat_pool }.should_not raise_exception(NoMethodError)
+    end
+
+    it "should return nil if none is specified" do
+      @virtual_server_nil.snat_pool.should be nil
+    end    
+
+    it "should return a SnatPool" do
+      pending
+    end
+  end
+
+  describe "fallback_persistence_profile method" do
+    it "should exists" do
+      lambda { @virtual_server.fallback_persistence_profile }.should_not raise_exception(NoMethodError)
+    end
+
+    it "should return an FallBackPersistenceProfile instance" do
+      pending
+    end    
+
+    it "should return an nil if none is specified" do
+      @virtual_server_nil.fallback_persistence_profile.should be(nil)
+    end    
+  end
+
+  describe "vlan method" do
+    it "should exists" do
+      lambda { @virtual_server.vlan }.should_not raise_exception(NoMethodError)
+    end
+
+    it "should return an instance of a VLANFilterList" do
+      vlan = @virtual_server.vlan
+      vlan.class.should be(IControl::Common::VLANFilterList)
+      @virtual_server_nil.vlan.vlans.class.should be(Array)
+      @virtual_server_nil.vlan.vlans.should be_empty
+      vlan.vlans.should_not be_empty
+      vlan.vlans.class.should be(Array)
+    end
   end
 end
