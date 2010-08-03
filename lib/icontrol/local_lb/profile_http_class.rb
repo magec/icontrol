2a411ac90d87f732d8aa4a47e28f8f46344ef769
Jose Fernandez (magec) <jfernandezperez@gmail.com>
2010-08-03 10:11:20 +0200

    Added profile_type

--- lib/icontrol/local_lb/profile_http_class.rb
+++ lib/icontrol/local_lb/profile_http_class.rb
@@ -5,6 +5,10 @@ class IControl::LocalLB::ProfileHttpClass
 
   set_id_name :profile_name
 
+  def profile_type
+    return IControl::LocalLB::ProfileType::PROFILE_TYPE_HTTPCLASS
+  end
+
   def pool
     self.pool_name.first
   end
