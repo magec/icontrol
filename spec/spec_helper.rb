852ba4fdb5c48fce069fa3efffdc67d43ceaf447
Jose Fernandez (magec) <jfernandezperez@gmail.com>
2010-08-20 09:15:56 +0200

    Minor changes

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,33 +5,50 @@ require "rake"
 require 'spec'
 require 'spec/autorun'
 require 'fakeweb'
+require 'ap'
 Savon::Request.log = false
 FakeWeb.allow_net_connect = false
 
 FileList["spec/fixtures/**/*.rb"].each { |fixture| require fixture }
 
-IControl.config[:base_url] = "https://localhost/iControl/iControlPortal.cgi"
-IControl.config[:user] = "test_user"
-IControl.config[:password] = "secret"
 
+def register_conversation(conversation)
+  response_file_names = []
+  file_name = ""
+  conversation.each do |op|
+    file_name = FileList["spec/fixtures/soap/xml/*#{op}_*request*"].first
+    response_file_names << file_name.gsub("request","response")
+  end
+  
+
+  FakeWeb.register_uri :post, EndpointHelper.soap_endpoint, File.read(file_name), response_file_names.map{ |i| {:body => File.read(i)}}
+ 
+end
 
 # Mapping of the wsdl's
-
-FileList["spec/fixtures/wsdl/xml/*"].each do |file_name|
-  klass = file_name.split("/").last.gsub(".xml","").to_s.gsub(".","::").to_s
-  FakeWeb.register_uri :get, EndpointHelper.wsdl_endpoint(klass), :body => File.read(file_name)
+def register_wsdls
+  puts "Registering wsdls"
+  FileList["spec/fixtures/wsdl/xml/*"].each do |file_name|
+    klass = file_name.split("/").last.gsub(".xml","").to_s.gsub(".","::").to_s
+    FakeWeb.register_uri :get, EndpointHelper.wsdl_endpoint(klass), :body => File.read(file_name)
+  end
 end
 
 # Mapping of the soap conversations
-
-FileList["spec/fixtures/soap/xml/*request*"].each do |file_name|
-  response_file_name = file_name.gsub("request","response")
-  request_body = File.read(file_name)
-  FakeWeb.register_uri :post, EndpointHelper.soap_endpoint, :body => File.read(response_file_name), :request_body => File.read(file_name)
+def register_fixtures
+  FileList["spec/fixtures/soap/xml/*request*"].each do |file_name|
+    response_file_name = file_name.gsub("request","response")
+    request_body = File.read(file_name)
+    FakeWeb.register_uri :post, EndpointHelper.soap_endpoint, File.read(file_name), :body => File.read(response_file_name)
+  end
 end
 
 Spec::Runner.configure do |config|
+  IControl.config[:base_url] = "https://localhost:443/iControl/iControlPortal.cgi"
+  IControl.config[:user] = "test_user"
+  IControl.config[:password] = "secret"
+
+  register_wsdls
+  register_fixtures
+#  FakeWeb::Registry.instance.uri_map.each {|k,v| ap k; v.each {|i,j| ap j.keys }}
 end
-
-
-
