ab9f85309a6ca504135d7310fda6726e8c29b90d
Jose Fernandez (magec) <jfernandezperez@gmail.com>
2010-08-20 09:14:18 +0200

    Added support for the new classes and name changes

--- lib/icontrol/mappings.rb
+++ lib/icontrol/mappings.rb
@@ -8,8 +8,8 @@ module IControl
       when /iControl:LocalLB\.MatchPatternString/ then [result[:item]].flatten.compact
       when /iControl:Common\.IPPortDefinition\[\]/ then [result[:item][:item]].flatten.map{|i| IControl::LocalLB::PoolMember.new(i)}
       when /iControl:LocalLB.LBMethod/ then [result[:item]].flatten
-      when /VirtualServerProfileAttribute\[\]/ then [result[:item][:item]].flatten.map{|i| IControl::LocalLB::VirtualServer::VirtualServerProfileAttribute.new(i) }
-      when /iControl:LocalLB.VirtualServer.VirtualServerHttpClass\[\]/ then [result[:item][:item]].flatten.compact.map{|i| IControl::LocalLB::ProfileHttpClass.new(i)}
+      when /VirtualServerProfileAttribute\[\]/ then [result[:item][:item]].flatten.map{|i| IControl::LocalLB::VirtualServer::ProfileAttribute.new(i) }
+      when /iControl:LocalLB.VirtualServer.VirtualServerHttpClass\[\]/ then [result[:item][:item]].flatten.compact.sort{|a,b| a[:priority] <=> b[:priority]}.map{ |i| IControl::LocalLB::ProfileHttpClass.new(i)}
       when /iControl:LocalLB\.ProfileString\[/ then [result[:item]].flatten.map do |i| 
           if i[:value].class == String
             IControl::LocalLB::Pool.new(i.merge({:id => i[:value] }))
@@ -20,8 +20,8 @@ module IControl
         end
       when /iControl:LocalLB\.Pool\.MonitorAssociation\[/ then IControl::LocalLB::MonitorRule.from_xml(result[:item][:monitor_rule])
       when /iControl:LocalLB\.ObjectStatus\[/ then (result[:item])
-      when /iControl:LocalLB.VirtualServer.VirtualServerType\[/ then IControl::LocalLB::VirtualServer::VirtualServerType.from_string(result[:item])
-      when /iControl:LocalLB.VirtualServer.VirtualServerCMPEnableMode\[/ then IControl::LocalLB::VirtualServer::VirtualServerCMPEnableMode.from_string(result[:item])
+      when /iControl:LocalLB.VirtualServer.VirtualServerType\[/ then IControl::LocalLB::VirtualServer::Type.from_string(result[:item])
+      when /iControl:LocalLB.VirtualServer.VirtualServerCMPEnableMode\[/ then IControl::LocalLB::VirtualServer::CMPEnableMode.from_string(result[:item])
       when /iControl:Common\.IPPortDefinition\[/ then IControl::Common::IPPortDefinition.new(result[:item])
       when /iControl:Common.ProtocolType\[/ then IControl::Common::ProtocolType.from_string(result[:item])
       when /iControl:Common.EnabledState\[/ then IControl::Common::EnabledState.from_string(result[:item])
@@ -30,11 +30,14 @@ module IControl
       when /iControl:LocalLB.HardwareAccelerationMode\[/ then IControl::LocalLB::HardwareAccelerationMode.from_string(result[:item])
       when /iControl:LocalLB.SnatType\[/ then IControl::LocalLB::SnatType.from_string(result[:item])
       when /iControl:Common.VLANFilterList\[/ then IControl::Common::VLANFilterList.new(result[:item])
-      when /iControl:LocalLB.VirtualServer.VirtualServerPersistence\[\]\[\d+\]/ then  result[:item][:item] && IControl::LocalLB::VirtualServer::VirtualServerPersistence.new(result[:item][:item])
+      when /iControl:LocalLB.VirtualServer.VirtualServerPersistence\[\]\[\d+\]/ then  result[:item][:item] && IControl::LocalLB::VirtualServer::Persistence.new(result[:item][:item])
       when /iControl:LocalLB.VirtualServer.VirtualServerRule\[\]\[\d+\]/ then [result[:item][:item]].flatten.compact.map{ |i| IControl::LocalLB::VirtualServerRule.new(i) }
-      when /iControl:LocalLB.VirtualServer.VirtualServerClonePool\[\]\[\d+\]/ then  [result[:item][:item]].flatten.compact.map{ |i| IControl::LocalLB::VirtualServer::VirtualServerClonePool.new(i) }
-      when /iControl:LocalLB.VirtualServer.VirtualServerStatisticEntry\[/ then IControl::LocalLB::VirtualServer::VirtualServerStatisticEntry.new(result)
+      when /iControl:LocalLB.VirtualServer.VirtualServerClonePool\[\]\[\d+\]/ then  [result[:item][:item]].flatten.compact.map{ |i| IControl::LocalLB::VirtualServer::ClonePool.new(i) }
+      when /iControl:LocalLB.VirtualServer.VirtualServerStatisticEntry\[/ then IControl::LocalLB::VirtualServer::StatisticEntry.from_xml(result)
+      when /iControl:LocalLB.VirtualServer.VirtualServerAuthentication\[/ then [result[:item][:item]].flatten.compact.sort{|a,b| a[:priority] <=> b[:priority]}.map{ |i|  IControl::LocalLB::ProfileAuth.new(i)}
       when /y:boolean\[/ then  result[:item]
+      when /iControl:LocalLB.VirtualServer.VirtualServerModuleScore\[/ then result[:item][:item] && IControl::LocalLB::VirtualServer::ModuleScore.new(result[:item])
+
       else
         raise "No type matching found (#{result[:array_type]})" 
       end
