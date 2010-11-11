245b3cc81652da99d4e890bbd64cd0b75cf4b4c9
Jose Fernandez (magec) <joseferper@gmail.com>
2010-11-11 19:09:40 +0100

    Added some predeclarations to avoid exceptions when loading the classes

--- lib/icontrol.rb
+++ lib/icontrol.rb
@@ -1,5 +1,30 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
-require 'icontrol/base'
-require 'icontrol/common'
-require 'icontrol/locallb'
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__),"icontrol","icontrol"))
 
+require 'icontrol/base'
+
+# Predeclares a couple of things in order to get loaded
+
+module IControl
+
+  module GlobalLB
+    class VirtualServerDefinitionSequence < IControl::Base::Sequence ; end
+  end
+
+  module System
+    class CPUMetricSequenceSequence < IControl::Base::SequenceSequence; end
+    class FanMetricSequenceSequence < IControl::Base::SequenceSequence; end
+    class PSMetricSequenceSequence  < IControl::Base::SequenceSequence; end
+    class TemperatureMetricSequenceSequence < IControl::Base::SequenceSequence; end 
+  end
+
+  module Management
+    class EventSubscription < IControl::Base
+      class EventType < IControl::Base::Enumeration; end
+      class ObjectType < IControl::Base::Enumeration; end
+    end
+  end
+
+end
+
+require 'icontrol/icontrol'
