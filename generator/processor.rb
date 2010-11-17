501678286bc79b3d80fcdfdc3a095b144d3368f1
Jose Fernandez (magec) <joseferper@gmail.com>
2010-11-17 19:35:57 +0100

    Changed

--- generator/processor.rb
+++ generator/processor.rb
@@ -4,12 +4,10 @@ require 'fileutils'
 require File.dirname(__FILE__) + '/ast_tree'
 DEST_DIR=ARGV[0]
 
-root = Marshal.load(File.open("Todo.dat").read)
+root = Marshal.load(File.open("result.dat").read)
 
 new_root = ModuleDeclaration.new
 
-#FileUtils.remove_dir(DEST_DIR,true)
-#Dir.mkdir(DEST_DIR)
 Dir.chdir(DEST_DIR)
 
 new_root.properties[:name] = "IControl"
