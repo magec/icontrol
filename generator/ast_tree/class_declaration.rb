6f0f30fb8f74b45ecfa7b6d521bfc53b743b768e
Jose Fernandez (magec) <joseferper@gmail.com>
2010-11-18 18:20:07 +0100

    Added support for spec tests when generating now, it also generates a sketeton for the tests

--- generator/ast_tree/class_declaration.rb
+++ generator/ast_tree/class_declaration.rb
@@ -3,7 +3,7 @@ class ClassDeclaration < ASTNode
   def parent_module
     parents(:of_class => ModuleDeclaration).map{ |i| i.properties[:name] }.reverse.join("::")
   end
-
+  
   def declare
     "class #{normalized_class_name} < IControl::Base; end"
   end
@@ -18,5 +18,26 @@ class ClassDeclaration < ASTNode
       super(file)
     end
     RBeautify.beautify_file(file_name)
+
+    spec_file_name = "#{normalized_file_name}_spec.rb"
+
+    spec_file_name = ((full_class_name + "Spec").decamelize + ".rb").split("/")
+    levels = full_class_name.split("::").map { |i| ".." }
+    
+    FileUtils.mkdir_p File.join(levels + ["spec"] + spec_file_name[0..-2])
+    File.open(File.join(levels + ["spec"] + spec_file_name),"w+") do |file|
+      render_spec(file)
+    end
+    
   end
+
+  def render_spec(file)
+
+    template_name = File.join(File.dirname(__FILE__),"..",TEMPLATES_DIR,"spec.erb")
+    template = ERB.new(File.open(template_name).read,nil,"<>")
+
+    file.puts(template.result(self.get_binding))
+  end
+
+
 end
