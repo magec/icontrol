fb21fd8f07078111698e311536ffa2f30a0acc7e
Jose Fernandez (magec) <jfernandezperez@gmail.com>
2010-12-22 14:24:03 +0100

    Now if no credentials are specified a message is shown

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,8 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+require "rubygems"
+require "bundler/setup"
+Bundler.setup(:test)
 
 require 'icontrol'
 require 'vcr'
@@ -14,12 +17,20 @@ VCR.config do |c|
 
 end
 
-Savon.log = false
+Savon.log = true
 
 RSpec.configure do |c|
+  
+
 
   c.extend VCR::RSpec::Macros
-  config_contents = File.read(File.join(File.dirname(__FILE__),"credentials.yml"))
+  begin
+    file_path = File.join(File.dirname(__FILE__),"credentials.yml")
+    config_contents = File.read(file_path)
+  rescue
+    puts "Error, could not read #{file_path}, please fill it with correct values (you have an example in #{file_path}.example)"
+    exit 1
+  end
   IControl.config = YAML.load(config_contents)
 
 end
