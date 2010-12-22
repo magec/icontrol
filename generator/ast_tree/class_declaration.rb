48e2c9578b3753c4e9b8c9b558538f264df012ea
Jose Fernandez (magec) <joseferper@gmail.com>
2010-12-22 18:05:50 +0100

    Now the classes define the id_name they have

--- generator/ast_tree/class_declaration.rb
+++ generator/ast_tree/class_declaration.rb
@@ -1,5 +1,11 @@
-class ClassDeclaration < ASTNode
+class ClassDeclaration < ASTNode  
+  
+  @@spec_mode = false
 
+  def self.spec_mode=(mode)
+    @@spec_mode=mode
+  end
+  
   def parent_module
     parents(:of_class => ModuleDeclaration).map{ |i| i.properties[:name] }.reverse.join("::")
   end
@@ -8,28 +14,47 @@ class ClassDeclaration < ASTNode
     "class #{normalized_class_name} < IControl::Base; end"
   end
 
+  def guess_id_name
+    methods = children_filtered(:of_class => MethodDeclaration)
+    counter = Hash.new(0)
+    max = { :name => "nil",:value => -1 }
+    methods.each do |method|
+      method.parameters.each do |parameter| 
+        name = parameter.properties[:name]
+        counter[name] = counter[name] + 1 
+        if counter[name] > max[:value]
+          max[:name] = name
+          max[:value] = counter[name]
+        end
+      end
+    end
+    return max[:name]
+  end
+
   def full_class_name
     parent_module + "::" + normalized_class_name
   end
 
   def compile(buffer = nil)
-    file_name = "#{normalized_file_name}.rb"
-    File.open(file_name,"w+") do |file|
-      super(file)
-    end
-    RBeautify.beautify_file(file_name)
+    unless @@spec_mode
+      file_name = "#{normalized_file_name}.rb"
+      File.open(file_name,"w+") do |file|
+        super(file)
+      end
+      RBeautify.beautify_file(file_name)
+    else
+      spec_file_name = "#{normalized_file_name}_spec.rb"
 
-    spec_file_name = "#{normalized_file_name}_spec.rb"
-
-    spec_file_name = ((full_class_name + "Spec").decamelize + ".rb").split("/")
-    levels = full_class_name.split("::").map { |i| ".." }
-    
-    FileUtils.mkdir_p File.join(levels + ["spec"] + spec_file_name[0..-2])
-    spec_file = File.join(levels + ["spec"] + spec_file_name)
-    File.open(spec_file,"w+") do |file|
-      render_spec(file)
+      spec_file_name = ((full_class_name + "Spec").decamelize + ".rb").split("/")
+      levels = full_class_name.split("::").map { |i| ".." }
+      
+      FileUtils.mkdir_p File.join(levels + ["spec"] + spec_file_name[0..-2])
+      spec_file = File.join(levels + ["spec"] + spec_file_name)
+      File.open(spec_file,"w+") do |file|
+        render_spec(file)
+      end
+      RBeautify.beautify_file(spec_file)
     end
-    RBeautify.beautify_file(spec_file)
   end
 
   def render_spec(file)
