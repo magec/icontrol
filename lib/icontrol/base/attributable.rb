2ccd21f891ae7054848114a2afeaf609bc2c49a4
Jose Fernandez (magec) <joseferper@gmail.com>
2010-12-22 18:08:58 +0100

    Simplified the implementation of the id_member class method

--- lib/icontrol/base/attributable.rb
+++ lib/icontrol/base/attributable.rb
@@ -1,58 +1,51 @@
 module IControl
 
-class Base
+  class Base
 
-  module Attributable # :nodoc: 
-    
-    def self.included(klass)
-      klass.class_eval do
-        extend ClassMethods
-        include InstanceMethods
-      end
-    end
-    
-    module ClassMethods # :nodoc:
-      def set_id_name(id_name)
-        @id_name = id_name
-      end
+    module Attributable # :nodoc: 
       
-      def id_name
-        @id_name
-      end
-    end
-    
-    module InstanceMethods # :nodoc:
-      
-      def id
-        @attributes[:id]
+      def self.included(klass)
+        klass.class_eval do
+          include InstanceMethods
+          class << self;
+            attr_accessor :id_name
+            def set_id_name(name)
+              @id_name = name
+            end
+          end
+        end
       end
 
-      def initialize(attributes)
-        id = attributes.delete(self.class.id_name) if attributes && attributes[self.class.id_name]
-        @attributes = attributes || {}
-        @attributes[:id] ||= id       
+      module InstanceMethods # :nodoc:
         
-        # Now we define an alias for the id_name
-      id_name = self.class.id_name
-
-      (class << self; self; end).instance_eval do        
-        define_method(id_name) do 
+        def id
           @attributes[:id]
         end
-      end if id_name
 
+        def initialize(attributes)
+          id = attributes.delete(self.class.id_name) if attributes && attributes[self.class.id_name]
+          @attributes = attributes || {}
+          @attributes[:id] ||= id       
+          
+          # Now we define an alias for the id_name
+          id_name = self.class.id_name
+
+          (class << self; self; end).instance_eval do        
+            define_method(id_name) do 
+              @attributes[:id]
+            end
+          end if id_name
+        end
+
+        def method_missing(method_name,*args,&block)
+          if @attributes && @attributes.has_key?(method_name)
+            return @attributes[method_name] 
+          else
+            super
+          end 
+        end  
+      end
 
     end
-
-    def method_missing(method_name,*args,&block)
-      if @attributes && @attributes.has_key?(method_name)
-        return @attributes[method_name] 
-      else
-        super
-      end 
-    end  
   end
-
-end
-end
 end
