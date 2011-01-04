76141fdf915452004bf300e21658ff84d1a7ec09
Jose Fernandez (magec) <joseferper@gmail.com>
2011-01-04 17:31:13 +0100

    Really solved the duplication bug   ee16b052613b105d38f0

--- lib/icontrol/base/enumeration.rb
+++ lib/icontrol/base/enumeration.rb
@@ -7,8 +7,9 @@ module IControl
           # WorkArround when there are two  values with the same key (even if this one is an attribute) the hash contains an array of values, 
           # instead of just one value. In this case we choose the one that does not starts with iControl (cause this usually happends when
           # type clashes with the attribute class type) this happends when calling IControl::LocalLB::Pool#monitor_association for example
-          value = value.find { |i| i !=~ /iControl/ } if value.respond_to?(:find)
-          
+
+          value = value.find { |i| i =~ /^[^iControl].*/ } if value.respond_to?(:find)
+
           return nil unless value
           return class_eval "#{self.name}::#{[*value].last}"
         end
