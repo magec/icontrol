e3c4665773af03e91635c401c1f005d1b733c053
Jose Fernandez (magec) <jfernandezperez@gmail.com>
2011-01-05 11:58:39 +0100

    Added overlay functionality so modifications can be made to the generated code

---
+++ lib/icontrol/base/icontrol_overlay.rb
@@ -0,0 +1,6 @@
+# Given that most of the source code is generated (for documentation pourposes)
+# We need a way to add methods and functionality without making those changes 
+# in the generated code, to  be ablo to regenerate it when neccesary. So as
+# ruby allows open classes we use them to add functionality This file will load those files at boot time
+$LOAD_PATH.unshift(File.dirname(__FILE__))
+require 'icontrol/local_lb/pool'
