3f3205d00b8d672fa7c64aac9206caa7a95c26c8
Jose Fernandez (magec) <jfernandezperez@gmail.com>
2011-01-05 11:22:00 +0100

    100% coverage at IControl::LocalLB::Pool

--- spec/icontrol/local_lb/pool_spec.rb
+++ spec/icontrol/local_lb/pool_spec.rb
@@ -4,20 +4,28 @@ describe IControl::LocalLB::Pool do
 
   use_vcr_cassette "IControl::LocalLB::Pool", :record => :all, :match_requests_on => [:uri, :method, :body] # Change :record => :new_episodes when done
 
+  TEST_POOL_NAME="test_pool"
+
   before(:each) do
- #   IControl::LocalLB::Pool.create(:pool_name => "test_pool",
-  #                                 :lb_method => IControl::LocalLB::LBMethod::LB_METHOD_ROUND_ROBIN,
-   #                                :members => [{:address => "192.168.50.1",:port => "80"}])
-    @pool = IControl::LocalLB::Pool.find("test_pool")
+    IControl::LocalLB::Pool.create(:pool_name => TEST_POOL_NAME,
+                                   :lb_method => IControl::LocalLB::LBMethod::LB_METHOD_ROUND_ROBIN,
+                                   :members => [{:address => "192.168.50.1",:port => "80"},
+                                                {:address => "192.168.50.2",:port => "80"},
+                                                {:address => "192.168.50.3",:port => "80"},
+                                                {:address => "192.168.50.4",:port => "80"},
+                                                {:address => "192.168.50.5",:port => "80"},
+                                                {:address => "192.168.50.6",:port => "80"}
+                                               ])
+    @pool = IControl::LocalLB::Pool.find(TEST_POOL_NAME)
   end
 
   after(:each) do
-#    @pool.delete_pool
+    @pool.delete_pool
   end
 
   describe "#add_member" do
     it "Adds members to the specified pools." do
-      lambda { @pool.add_member(:members => [{:address => "192.168.50.1",:port => "80"}]) }.should_not raise_exception
+      lambda { @pool.add_member(:members => [{:address => "192.168.51.1",:port => "80"}]) }.should_not raise_exception
     end
 
     it "works this way" do
@@ -51,21 +59,31 @@ describe IControl::LocalLB::Pool do
 
   describe "#delete_persistence_record" do
     it "Deletes the persistence records based on the specified persistent modes for the specified pools." do
-      pending
-    end
-
-    it "works this way" do
-      pending
+      pending("poorly documented, idem")
     end
   end
 
   describe "#delete_pool" do
     it "Deletes the specified pools." do
-      pending
+      IControl::LocalLB::Pool.create(:pool_name => "to_be_deleted",
+                                       :lb_method => IControl::LocalLB::LBMethod::LB_METHOD_ROUND_ROBIN,
+                                       :members => [{:address => "192.168.50.1",:port => "80"}])
+
+      @deletion_pool = IControl::LocalLB::Pool.find("to_be_deleted")
+      
+      lambda { @deletion_pool.delete_pool }.should_not raise_exception
+      IControl::LocalLB::Pool.find("to_be_deleted").should be_nil
     end
 
     it "works this way" do
-      pending
+      IControl::LocalLB::Pool.create(:pool_name => "to_be_deleted",
+                                       :lb_method => IControl::LocalLB::LBMethod::LB_METHOD_ROUND_ROBIN,
+                                       :members => [{:address => "192.168.50.1",:port => "80"}])
+      # Given a pool
+      @my_pool = IControl::LocalLB::Pool.find("to_be_deleted")
+      
+      # Just calling delete_pool will erase it
+      @my_pool.delete_pool
     end
   end
 
@@ -84,7 +102,7 @@ describe IControl::LocalLB::Pool do
     end
 
     it "returns an instance of IControl::LocalLB::ServiceDownAction" do
-      IControl::LocalLB::ServiceDownAction.constants.should include(@pool.action_on_service_down.to_s)
+      lambda { IControl::LocalLB::ServiceDownAction.const_get(@pool.action_on_service_down) }.should_not raise_exception
     end
   end
 
@@ -147,8 +165,8 @@ describe IControl::LocalLB::Pool do
       # => #<IControl::Common::TimeStamp:0x9ddcf6c @attributes={:year=>"2011", :month=>"1", :day=>"4", :hour=>"10", :minute=>"59", :second=>"29", :id=>nil}>
 
 
-      test_pool_statistics = all_statistics.statistics.find { |i| i.pool_name == "test_pool" }
-      # => #<IControl::LocalLB::Pool::PoolStatisticEntry:0xa492f8c @attributes={:pool_name=>"test_pool", :statistics=>["<.....   The statistics for a given pool
+      test_pool_statistics = all_statistics.statistics.find { |i| i.pool_name == TEST_POOL_NAME }
+      # => #<IControl::LocalLB::Pool::PoolStatisticEntry:0xa492f8c @attributes={:pool_name=>TEST_POOL_NAME, :statistics=>["<.....   The statistics for a given pool
       test_pool_statistics.statistics
       # => [#<IControl::Common::Statistic:0xa497d34 @attributes={:type=>:STATISTIC_SERVER_SIDE_BYTES_IN, :value=>#<IControl::Common::ULong64:0xa497d98 @attributes={:high=>"0", :low=>"0", :id=>nil}>, :time_stamp=>"0", :id=>nil}>, #<IControl::Common::Statistic:0xa497780.... The actual values    
 
@@ -174,7 +192,7 @@ describe IControl::LocalLB::Pool do
     end
 
     it "returns an instance of IControl::LocalLB::EnabledState" do
-      IControl::Common::EnabledState.constants.should include(@pool.allow_nat_state.to_s)
+      lambda { IControl::Common::EnabledState.const_get(@pool.allow_nat_state.to_s) }.should_not raise_exception
     end
   end
 
@@ -193,7 +211,7 @@ describe IControl::LocalLB::Pool do
     end
 
     it "returns an instance of EnabledState" do
-      IControl::Common::EnabledState.constants.should include(@pool.allow_snat_state.to_s)
+      lambda { IControl::Common::EnabledState.const_get(@pool.allow_snat_state.to_s) }.should_not raise_exception
     end
   end
 
@@ -271,7 +289,7 @@ describe IControl::LocalLB::Pool do
     end
 
     it "returns an instance of IControl::LocalLB::LBMethod" do
-      IControl::LocalLB::LBMethod.constants.should include(@pool.lb_method.to_s)
+      lambda { IControl::LocalLB::LBMethod.const_get(@pool.lb_method.to_s) }.should_not raise_exception
     end
   end
 
@@ -281,15 +299,7 @@ describe IControl::LocalLB::Pool do
     end
 
     it "Gets a list of all pools." do
-      pending
-    end
-
-    it "works this way" do
-      pending
-    end
-
-    it "returns an instance of String" do
-      pending
+      pending("Waiting to see the rest of the examples, maybe it would be convenient to return a list of instances instead of just strings")
     end
   end
 
@@ -367,7 +377,7 @@ describe IControl::LocalLB::Pool do
     end
 
     it "returns an instance of IControl::Common::HAAction" do
-      IControl::Common::HAAction.constants.should include(@pool.minimum_up_member_action.to_s)
+      lambda { IControl::Common::HAAction.const_get(@pool.minimum_up_member_action.to_s) }.should_not raise_exception
     end
   end
 
@@ -386,7 +396,7 @@ describe IControl::LocalLB::Pool do
     end
 
     it "returns an instance of IControl::Common::EnabledState" do
-      IControl::Common::EnabledState.constants.should include(@pool.minimum_up_member_enabled_state.to_s)
+      lambda { IControl::Common::EnabledState.const_get(@pool.minimum_up_member_enabled_state.to_s) }.should_not raise_exception
     end
   end
 
@@ -409,15 +419,22 @@ describe IControl::LocalLB::Pool do
   end
 
   describe "#monitor_instance" do
+
+    before(:each) do
+      IControl::LocalLB::Pool.set_monitor_association(:monitor_association => {:pool_name => TEST_POOL_NAME,:monitor_rule => {:type => IControl::LocalLB::MonitorRuleType::MONITOR_RULE_TYPE_SINGLE,:monitor_templates => ["tcp"],:quorum => 0} })
+    end
+    
     it "should return without raising any exception" do
       lambda { @pool.monitor_instance }.should_not raise_exception
     end
 
     it "Gets the monitor instance information for the specified pools, i.e. the monitor instance information for the pool members of the specified pools." do
+      # for this to work a monitor have to be associated to the pool
       @pool.monitor_instance.should_not be_nil
     end
 
     it "works this way" do
+
       monitor_states = @pool.monitor_instance
       monitor_states.first.instance_state
       # => :INSTANCE_STATE_DOWN  (The actual state of the first member)
@@ -459,15 +476,7 @@ describe IControl::LocalLB::Pool do
   describe "#persistence_record" do
 
     it "Gets the persistence records based on the specified persistent modes for the specified pools." do
-      pending
-    end
-
-    it "works this way" do
-      pending
-    end
-
-    it "returns an instance of PersistenceRecord[]" do
-      pending
+      pending("poorly documented, no responses are received when trying")
     end
   end
 
@@ -610,181 +619,189 @@ describe IControl::LocalLB::Pool do
 
   describe "#set_monitor_association" do
     it "Sets/creates the monitor associations for the specified pools. This basically creates the monitor associations between a pool and a monitor rule." do
-      IControl::LocalLB::Pool.set_monitor_association(:monitor_association => {:pool_name => "test_pool",:monitor_rule => {:type => IControl::LocalLB::MonitorRuleType::MONITOR_RULE_TYPE_SINGLE,:quorum => 0,:monitor_templates => "tcp"}} )
+      lambda { IControl::LocalLB::Pool.set_monitor_association(:monitor_association => {:pool_name => TEST_POOL_NAME,:monitor_rule => {:type => IControl::LocalLB::MonitorRuleType::MONITOR_RULE_TYPE_SINGLE,:monitor_templates => ["tcp"],:quorum => 0} })}.should_not raise_exception
     end
-
+    
     it "works this way" do
-      pending
+      IControl::LocalLB::Pool.set_monitor_association(:monitor_association => 
+                                                      {:pool_name => TEST_POOL_NAME,
+                                                        :monitor_rule => {
+                                                          :type => IControl::LocalLB::MonitorRuleType::MONITOR_RULE_TYPE_SINGLE,
+                                                          :monitor_templates => ["http"],
+                                                          :quorum => 0}
+                                                      }).inspect
+      # Note that this does not add a new monitor association but replaces the one currently set
     end
   end
 
   describe "#remove_monitor_association" do
     it "Removes the monitor associations for the specified pools. This basically deletes the monitor associations between a pool and a monitor rule, i.e. the specified pools will no longer be monitored." do
-      pending
+      lambda { @pool.remove_monitor_association }.should_not raise_exception
     end
-
+    
     it "works this way" do
-      pending
+      @pool.remove_monitor_association
+      # => nil (every association was removed unless exception thrown)
     end
   end
 
   describe "#reset_statistics" do
     it "Resets the statistics for the specified pools." do
-      pending
+      lambda { @pool.reset_statistics }.should_not raise_exception
     end
 
     it "works this way" do
-      pending
+      @pool.reset_statistics
+      # => Statistics reset unless exception thrown
     end
   end
 
   describe "#set_action_on_service_down" do
     it "Sets the action to take when the node goes down for the specified pools." do
-      pending
+      lambda { @pool.set_action_on_service_down(:action => IControl::LocalLB::ServiceDownAction::SERVICE_DOWN_ACTION_RESET) }.should_not raise_exception
     end
 
     it "works this way" do
-      pending
+      @pool.set_action_on_service_down(:action => IControl::LocalLB::ServiceDownAction::SERVICE_DOWN_ACTION_RESET) 
+      # Available Actions are: SERVICE_DOWN_ACTION_NONE, SERVICE_DOWN_ACTION_RESET, SERVICE_DOWN_ACTION_DROP, SERVICE_DOWN_ACTION_RESELECT 
     end
   end
 
   describe "#set_allow_nat_state" do
     it "Sets the states indicating whether NATs are allowed for the specified pools." do
-      pending
+      lambda { @pool.set_allow_nat_state(:state => IControl::Common::EnabledState::STATE_DISABLED) }.should_not raise_exception
     end
 
     it "works this way" do
-      pending
+      @pool.set_allow_nat_state(:state => IControl::Common::EnabledState::STATE_DISABLED)
     end
   end
 
   describe "#set_allow_snat_state" do
     it "Sets the states indicating whether SNATs are allowed for the specified pools." do
-      pending
+      lambda { @pool.set_allow_snat_state(:state => IControl::Common::EnabledState::STATE_DISABLED) }.should_not raise_exception
     end
 
     it "works this way" do
-      pending
+      @pool.set_allow_snat_state(:state => IControl::Common::EnabledState::STATE_DISABLED)
     end
   end
 
   describe "#set_client_ip_tos" do
     it "Sets the IP ToS values for client traffic for the specified pools." do
-      pending
+      lambda { @pool.set_client_ip_tos(:value => 1) }.should_not raise_exception
     end
 
     it "works this way" do
-      pending
+      @pool.set_client_ip_tos(:value => 1)
     end
   end
 
   describe "#set_client_link_qos" do
     it "Sets the link QoS values for client traffic for the specified pools." do
-      pending
+      lambda { @pool.set_client_link_qos(:value => 1) }.should_not raise_exception
     end
 
     it "works this way" do
-      pending
-    end
-  end
-
-  describe "#set_gateway_failsafe_unit_id" do
-    it "Sets the gateway failsafe unit IDs for the specified pools." do
-      pending
-    end
-
-    it "works this way" do
-      pending
-    end
-  end
-
-  describe "#set_lb_method" do
-    it "Sets the load balancing methods for the specified pools." do
-      pending
-    end
-
-    it "works this way" do
-      pending
-    end
-  end
-
-  describe "#set_minimum_active_member" do
-    it "Sets the minimum active member counts for the specified pools." do
-      pending
-    end
-
-    it "works this way" do
-      pending
-    end
-  end
-
-  describe "#set_minimum_up_member" do
-    it "Sets the minimum member counts that are required to be UP for the specified pools." do
-      pending
-    end
-
-    it "works this way" do
-      pending
-    end
-  end
-
-  describe "#set_minimum_up_member_action" do
-    it "Sets the actions to be taken if the minimum number of members required to be UP for the specified pools is not met." do
-      pending
-    end
-
-    it "works this way" do
-      pending
-    end
-  end
-
-  describe "#set_minimum_up_member_enabled_state" do
-    it "Sets the states indicating that the feature that requires a minimum number of members to be UP is enabled/disabled for the specified pools." do
-      pending
-    end
-
-    it "works this way" do
-      pending
+      @pool.set_client_link_qos(:value => 0)
     end
   end
 
   describe "#set_server_ip_tos" do
     it "Sets the IP ToS values for server traffic for the specified pools." do
-      pending
+      lambda { @pool.set_server_ip_tos(:value => 1) }.should_not raise_exception
     end
 
     it "works this way" do
-      pending
+      @pool.set_server_ip_tos(:value => 1)
     end
   end
 
   describe "#set_server_link_qos" do
     it "Sets the link QoS values for server traffic for the specified pools." do
-      pending
+      lambda { @pool.set_server_link_qos(:value => 1) }.should_not raise_exception
     end
 
     it "works this way" do
-      pending
+      @pool.set_server_link_qos(:value => 0)
+    end
+  end
+
+  describe "#set_gateway_failsafe_unit_id" do
+    it "Sets the gateway failsafe unit IDs for the specified pools." do
+      lambda { @pool.set_gateway_failsafe_unit_id(:unit_id => 1) }.should_not raise_exception
+    end
+
+    it "works this way" do
+      @pool.set_gateway_failsafe_unit_id(:unit_id => 1)
+    end
+  end
+
+  describe "#set_lb_method" do
+    it "Sets the load balancing methods for the specified pools." do
+      lambda { @pool.set_lb_method(:lb_method => IControl::LocalLB::LBMethod::LB_METHOD_RATIO_MEMBER) }.should_not raise_exception
+    end
+
+    it "works this way" do
+      @pool.set_lb_method(:lb_method => IControl::LocalLB::LBMethod::LB_METHOD_RATIO_MEMBER) 
+       # => 
+     end
+   end
+
+  describe "#set_minimum_active_member" do
+    it "Sets the minimum active member counts for the specified pools." do
+      lambda { @pool.set_minimum_active_member(:value => 5) }.should_not raise_exception
+    end
+    
+    it "works this way" do
+      @pool.set_minimum_active_member(:value => 5)
+    end
+  end
+
+  describe "#set_minimum_up_member" do
+    it "Sets the minimum member counts that are required to be UP for the specified pools." do
+      lambda { @pool.set_minimum_up_member(:value => 4) }.should_not raise_exception
+    end
+
+    it "works this way" do
+      @pool.set_minimum_up_member(:value => 4)
+    end
+  end
+
+  describe "#set_minimum_up_member_action" do
+    it "Sets the actions to be taken if the minimum number of members required to be UP for the specified pools is not met." do
+#      lambda { @pool.set_minimum_up_member_action(:action => IControl::Common::HAAction::HA_ACTION_NONE) }.should_not raise_exception
+      pending("poorly documented, what does failover or reset mean in this context?")
+    end
+  end
+
+  describe "#set_minimum_up_member_enabled_state" do
+    it "Sets the states indicating that the feature that requires a minimum number of members to be UP is enabled/disabled for the specified pools." do
+      lambda { @pool.set_minimum_up_member_enabled_state(:state => IControl::Common::EnabledState::STATE_DISABLED) }.should_not raise_exception
+    end
+
+    it "works this way" do
+      @pool.set_minimum_up_member_enabled_state(:state => IControl::Common::EnabledState::STATE_DISABLED)
     end
   end
 
   describe "#set_simple_timeout" do
     it "Sets the simple timeouts for the specified pools." do
-      pending
+      lambda { @pool.set_simple_timeout(:simple_timeout => 5) }.should_not raise_exception
     end
 
     it "works this way" do
-      pending
+      @pool.set_simple_timeout(:simple_timeout => 5)
     end
   end
 
   describe "#set_slow_ramp_time" do
     it "Sets the ramp-up time (in seconds) to gradually ramp up the load on newly added or freshly detected UP pool members." do
-      pending
+      lambda { @pool.set_slow_ramp_time(:value => 5) }.should_not raise_exception
     end
 
     it "works this way" do
-      pending
+      @pool.set_slow_ramp_time(:value => 5)
     end
   end
 end
