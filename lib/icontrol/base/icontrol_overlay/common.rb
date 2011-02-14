9712ed6a2e673a3711e5aeecb7fd1130319cc143
Jose Fernandez (magec) <jfernandezperez@gmail.com>
2011-02-14 11:21:01 +0100

    Solved a bug it to_f for ULong values

--- lib/icontrol/base/icontrol_overlay/common.rb
+++ lib/icontrol/base/icontrol_overlay/common.rb
@@ -3,15 +3,15 @@ module IControl
     class ULong64 < IControl::Base::Struct
       def to_f
         retVal = 0.0
-        if @high >=0
-          retVal = @high << 32 & 0xffff0000
+        if high >=0
+          retVal = high << 32 & 0xffff0000
         else
-          retVal = ((@high & 0x7fffffff) << 32) + (0x80000000 << 32)
+          retVal = ((high & 0x7fffffff) << 32) + (0x80000000 << 32)
         end
-        if @low >=0
-          retVal += @low
+        if low >=0
+          retVal += low
         else
-          retVal += ((@low & 0x7fffffff) + 0x7fffffff)
+          retVal += ((low & 0x7fffffff) + 0x7fffffff)
         end
         return retVal;
       end    
