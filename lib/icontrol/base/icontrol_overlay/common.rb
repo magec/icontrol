e06ab3efec9574bb007f2fc12efc321931a3045e
Jose Fernandez (magec) <jfernandezperez@gmail.com>
2011-03-01 09:56:21 +0100

    Solved a bug that did not convert ULong64 correctly

--- lib/icontrol/base/icontrol_overlay/common.rb
+++ lib/icontrol/base/icontrol_overlay/common.rb
@@ -1,17 +1,30 @@
+require 'bindata'
 module IControl
   module Common 
     class ULong64 < IControl::Base::Struct
+
+      def from_soap(xml)
+        puts "HEY"
+        puts xml.inspect
+      end
+      
       def to_f
-        retVal = 0.0
-        if high >=0
-          retVal = high << 32 & 0xffff0000
+        BinData::Uint64be.read(BinData::Int32be.new(high).to_binary_s + BinData::Int32be.new(low).to_binary_s)
+      end
+
+      def to_fd
+        retVal = 0.0        
+        u_high = high.to_s.unpack("L").first || 0
+        u_low  = low.to_s.unpack("L").first  || 0
+        if u_high >=0
+          retVal = u_high << 32 & 0xffff0000
         else
-          retVal = ((high & 0x7fffffff) << 32) + (0x80000000 << 32)
+          retVal = ((u_high & 0x7fffffff) << 32) + (0x80000000 << 32)
         end
-        if low >=0
-          retVal += low
+        if u_low >=0
+          retVal += u_low
         else
-          retVal += ((low & 0x7fffffff) + 0x7fffffff)
+          retVal += ((u_low & 0x7fffffff) + 0x7fffffff)
         end
         return retVal;
       end    
--- spec/icontrol/common_spec.rb
+++ spec/icontrol/common_spec.rb
@@ -1,10 +1,15 @@
 require File.expand_path(File.join(File.dirname(__FILE__),"..",'/spec_helper'))
 
 describe IControl::Common::ULong64 do
-  
+  use_vcr_cassette "IControl::Common::ULong64", :record => :all, :match_requests_on => [:uri, :method, :body] # Change :record => :new_episodes when done
   describe "#to_f" do
     it "should exist" do
       IControl::Common::ULong64.new({}).methods.should include(:to_f)
+    end
+
+    it "should correctly perform the conversion between two unsigned ints and a bignum" do
+      IControl::Common::ULong64.new(:high => 0, :low => 37565545).to_f.should == 37565545
+      IControl::Common::ULong64.new(:high => 2, :low => -1701380642).to_f.should == 11183521246
     end
   end
 
